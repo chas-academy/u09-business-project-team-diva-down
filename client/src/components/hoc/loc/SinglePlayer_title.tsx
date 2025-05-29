5b76ef1d3b3aa41197eeff41871cbcee3b9a224d
David Segerbo <david.k.segerbo@gmail.com>
2025-05-30 00:38:06 +0200

    updated homepage, working on singleplayer gameloop

---
+++ client/src/components/hoc/loc/SinglePlayer_title.tsx
@@ -0,0 +1,12 @@
+import title_comp from "../title_comp";
+
+const singleplayer_title_info = () => {
+    return (
+        <>
+            <h2 id="singleplayer_main_title">Single player</h2>
+            <p>Experience trivia like never before with our sleek interface and vibrant neon accents. Challenge yourself with thousands of questions that glow in the dark.</p>
+        </>
+    );
+};
+
+export default title_comp(singleplayer_title_info);
