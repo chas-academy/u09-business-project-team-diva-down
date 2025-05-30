8ec1ef1774915418e79b17d89dcf126eba1abef4
David Segerbo <david.k.segerbo@gmail.com>
2025-05-30 06:25:47 +0200

    almost finnsihed with the single player settings

--- client/src/components/common/gameloop/settings_card.tsx
+++ client/src/components/common/gameloop/settings_card.tsx
@@ -1,30 +1,94 @@
-import Countdown from "../CountDownTimer";
+import { useState } from "react";
 import Home from "../../hoc/loc/Home_button";
 import Play from "../../hoc/loc/Play.button";
 import Category_Dropdown from "./category_dropdown";
 
-const gameloop_settings_card = () => {
+const GameloopSettingsCard = () => {
+  const [difficulty, setDifficulty] = useState("");
+  const [ranked, setRanked] = useState("");
 
-    return (
-        <>
-            <div className="settings-container">
-                <div className="settings-bar">
-                    <h2>Category</h2>
-                    <Category_Dropdown />
-                </div>
-                <div className="settings-bar">
-                    <h2>Difficulty</h2>
-                </div>
-                <div className="settings-bar">
-                    <h2>Ranked</h2>
-                </div>
-                <div className="settings-bar empty">
-                    <Home />
-                    <Play />
-                </div>
-            </div>
-        </>
-    );
+  function checkStatus() {
+    console.log(difficulty);
+    console.log(ranked);
+  }
+
+  return (
+    <div className="settings-container">
+      <div className="settings-bar">
+        <h2>Category</h2>
+        <Category_Dropdown />
+      </div>
+      <div className="settings-bar">
+        <h2>Difficulty</h2>
+        <div className="radio-options">
+          <label className={`input-styling ${difficulty === "Easy" ? "checked" : ""}`}>
+            <input
+              className="hidden"
+              type="radio"
+              name="difficulty"
+              value="Easy"
+              checked={difficulty === "Easy"}
+              onChange={(e) => setDifficulty(e.target.value)}
+            />
+            Easy
+          </label>
+          <label className={`input-styling ${difficulty === "Medium" ? "checked" : ""}`}>
+            <input
+              className="hidden"
+              type="radio"
+              name="difficulty"
+              value="Medium"
+              checked={difficulty === "Medium"}
+              onChange={(e) => setDifficulty(e.target.value)}
+            />
+            Medium
+          </label>
+          <label className={`input-styling ${difficulty === "Hard" ? "checked" : ""}`}>
+            <input
+              className="hidden"
+              type="radio"
+              name="difficulty"
+              value="Hard"
+              checked={difficulty === "Hard"}
+              onChange={(e) => setDifficulty(e.target.value)}
+            />
+            Hard
+          </label>
+        </div>
+      </div>
+      <div className="settings-bar">
+        <h2>Ranked</h2>
+        <div className="radio-options">
+            <label className={`input-styling ${ranked === "Yes" ? "checked" : ""}`}>
+            <input
+              className="hidden"
+              type="radio"
+              name="difficulty"
+              value="Yes"
+              checked={ranked === "Yes"}
+              onChange={(e) => setRanked(e.target.value)}
+            />
+            Yes
+            </label>
+            <label className={`input-styling ${ranked === "No" ? "checked" : ""}`}>
+            <input
+              className="hidden"
+              type="radio"
+              name="difficulty"
+              value="No"
+              checked={ranked === "No"}
+              onChange={(e) => setRanked(e.target.value)}
+            />
+            No
+            </label>
+        </div>
+      </div>
+      <div className="settings-bar empty">
+        <Home />
+        <Play onClick={checkStatus}/>
+      </div>
+    </div>
+  );
 };
 
-export default gameloop_settings_card;
+export default GameloopSettingsCard;
