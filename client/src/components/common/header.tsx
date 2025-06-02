af14772ddb683593812e114146b60e224f5ed39e
David Segerbo <david.k.segerbo@gmail.com>
2025-06-02 07:05:46 +0200

    updated userdashboard into a static site, has responsiveness as well

--- client/src/components/common/header.tsx
+++ client/src/components/common/header.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Login from "../hoc/loc/login_button";
 import { Link } from "react-router-dom";
 import { RouterContainer } from "../../routes/RouteContainer";
+import { id } from "../../common/GenerateUserId";
 
 const Header = () => {
 
@@ -39,8 +40,6 @@ const Header = () => {
         ChangeTheme(e); // Passing the event to EChangeTheme where it's actually used
     };
 
-
-
     return (
         <>
             <header className="navbar">
@@ -48,7 +47,8 @@ const Header = () => {
                 <ul className="navlinks">
                     <Link to={RouterContainer.Homepage}>Home</Link>
                     <Link to={RouterContainer.SinglePlayer}>Play</Link>
-                    <li>Categories</li>
+                    {/* Temporary Link */}
+                    <Link to={RouterContainer.UserDashboard.replace(':id', id)}>Player Stats</Link>
                     <li>Leaderboard</li>
                     <li>About</li>
                 </ul>
